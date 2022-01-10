27e69925ec766efde82e6c8edb708a31f807ad46
Temolzin Roldan <troldan@athenea.com.mx>
2022-01-10 12:48:04 -0600

    First commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        try {
+            $products = Product::all();
+
+            return response()->json([
+                'success' => true,
+                'products' => $products,
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            $product = new Product();
+            $product->name = $request->input('name');
+            $product->price = $request->input('price');
+            $product->save();
+
+            DB::commit();
+
+            return response()->json([
+				'success' => true,
+				'product' => $product,
+			], 200);
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json([
+				'success' => false,
+				'message' => $e->getMessage()
+			]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            DB::beginTransaction();
+            $product = Product::find($id);
+            $product->fill($request->all());
+            $product->save();
+
+            DB::commit();
+
+            return response()->json([
+				'success' => true,
+				'product' => $product,
+			], 200);
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json([
+				'success' => false,
+				'message' => $e->getMessage()
+			]);
+        }
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            DB::beginTransaction();
+            $product = Product::where('id', $id)->first();
+            $product->delete();
+
+            DB::commit();
+            return response()->json([
+				'success' => true,
+			], 200);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+				'success' => false,
+				'message' => $e->getMessage()
+			]);
+        }
+    }
+}
