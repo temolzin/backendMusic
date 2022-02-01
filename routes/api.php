682de41b00b21b44242f2cba3d2246a450130f68
AlbertoGuzman <2519160038jguzmang@gmail.com>
2022-01-31 19:33:50 -0600

    login with gmail

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SocialAuthController;
+use App\Http\Controllers\VerificationController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -20,5 +22,11 @@ Route::get('/greeting', function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::get('/refresh', [AuthController::class, 'refresh']);
 
+    Route::get('/authorize/google/redirect', [SocialAuthController::class, 'redirectToProvider']);
+    Route::get('/authorize/google/callback', [SocialAuthController::class, 'handlesProviderCallback']);
+
     Route::resource('/product', ProductController::class);
 
+
+    
+    Route::get('/email-verification', [VerificationController::class, 'verify']);
