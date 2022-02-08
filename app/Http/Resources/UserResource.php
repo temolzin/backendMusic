60149bb2031e6d5442e17b6481c57a1f627bfadb
AlbertoGuzman <2519160038jguzmang@gmail.com>
2022-02-08 12:34:47 -0600

    Role and permissions creation

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        $userRoles = $this->roles()->with('permissions')->get();
+        $roles = $userRoles->pluck('slug');
+        $rolesPermissions = $userRoles->pluck('permissions')->flatten(1)->pluck('slug');
+        $userPermissions = $rolesPermissions->merge($this->permissions->pluck('slug'));
+
+        return [
+            //'id' => $this->id,
+            'name'        => $this->name,
+            'email'       => $this->email,
+            'created_at'  => $this->created_at->format('d-m-Y'),
+            'role'        => $roles,
+            "permissions" => $userPermissions,
+        ];
+    }
+}
