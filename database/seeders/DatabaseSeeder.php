8b299a53fa0b952f7429d48a73333fe785e5b31f
AlbertoGuzman <2519160038jguzmang@gmail.com>
2022-02-23 10:55:44 -0600

    Storage delete and Create image storage

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,8 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        
-       // $this->call(PermissionSeeder::class);
+
+        // $this->call(PermissionSeeder::class);
+        Storage::deleteDirectory('public/user_profile');
+        Storage::makeDirectory('public/user_profile');
+
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
     }
