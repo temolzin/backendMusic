dea7872d890d79df1f3c2bd57c92a7d50c9967a7
Alan Cid <alanciddiaz014@gmail.com>
2026-05-20 19:17:52 -0600

    feature: add styles to the email.

--- resources/views/mails/newsletter.blade.php
+++ resources/views/mails/newsletter.blade.php
@@ -1,8 +1,42 @@
-<html>
+<!DOCTYPE html>
+<html lang="es">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $subject }}</title>
+    <style>
+{{ file_get_contents(resource_path('css/app.css')) }}
+    </style>
 </head>
 <body>
-    {{ $content }}
+    <table class="newsletter-shell" role="presentation" width="100%" cellspacing="0" cellpadding="0" border="0">
+        <tr>
+            <td align="center" class="newsletter-shell__outer">
+                <table class="newsletter-card" role="presentation" width="100%" cellspacing="0" cellpadding="0" border="0">
+                    <tr>
+                        <td class="newsletter-hero">
+                            <img src="{{ asset('images/logo.png') }}" alt="Música GSM" class="newsletter-logo">
+                            <p class="newsletter-kicker">Música GSM</p>
+                            <h1 class="newsletter-title">{{ $subject }}</h1>
+                            <p class="newsletter-subtitle">Novedades, avisos y contenido especial para tu comunidad.</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="newsletter-content">
+                            <div class="newsletter-body">
+                                {!! $content !!}
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="newsletter-footer">
+                            <p>Recibiste este correo porque formas parte de la comunidad de Música GSM.</p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
