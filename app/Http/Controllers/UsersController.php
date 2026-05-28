06c29bdf686a5e486fabf1f64a23c5831c6e3382
Alan Cid <alanciddiaz014@gmail.com>
2026-05-28 13:53:08 -0600

    refactor: implement earlier return

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -106,29 +106,29 @@ class UsersController extends Controller
             $name = $request->input("name");
             $email = $request->input("email");
 
-            if (!empty($name) && !empty($email)) {
-
-                $valiEmail = User::where('email', $email)->Where('id', '!=', Auth::user()->id)->first();
-                if (!empty($valiEmail['email'])) {
-                    return response()->json(['message' => 'El correo electrónico ya esta en uso'], 401);
-                }
-
-                DB::beginTransaction();
-                $user = User::find(Auth::user()->id);
-                $user->name = $name;
-                $user->email = $email;
-                $user->save();
-                DB::commit();
-
-                return response()->json([
-                    'success' => true,
-                    'message' => 'Usuario actualizado',
-                ], 200);
-            } else
+            if (empty($name) || empty($email)) {
                 return response()->json([
                     'success' => false,
                     'message' => 'Error por campos vacíos',
                 ], 401);
+            }
+
+            $valiEmail = User::where('email', $email)->Where('id', '!=', Auth::user()->id)->first();
+            if (!empty($valiEmail['email'])) {
+                return response()->json(['message' => 'El correo electrónico ya esta en uso'], 401);
+            }
+
+            DB::beginTransaction();
+            $user = User::find(Auth::user()->id);
+            $user->name = $name;
+            $user->email = $email;
+            $user->save();
+            DB::commit();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Usuario actualizado',
+            ], 200);
         } catch (\Exception $e) {
             DB::rollback();
             return response()->json([
@@ -152,37 +152,37 @@ class UsersController extends Controller
             $confirmPassword = $request->input("confirmPassword");
             $currentPassword = $request->input("currentPassword");
 
-            if (!empty($newPassword) && !empty($confirmPassword) && !empty($currentPassword)) {
-                if ($newPassword != $confirmPassword) {
-                    return response()->json([
-                        'success' => false,
-                        'message' => 'Las contraseñas no coinciden',
-                    ], 401);
-                } else {
-
-                    if (Hash::check($currentPassword, Auth::user()->password)) {
-                        DB::beginTransaction();
-                        $user = User::find(Auth::user()->id);
-                        $user->password = Hash::make($newPassword);
-                        $user->save();
-                        DB::commit();
-
-                        return response()->json([
-                            'success' => true,
-                            'message' => 'Contraseña actualizada',
-                        ], 200);
-                    } else {
-                        return response()->json([
-                            'success' => false,
-                            'message' => 'La contraseña actual no coincide',
-                        ], 401);
-                    }
-                }
-            } else
+            if (empty($newPassword) || empty($confirmPassword) || empty($currentPassword)) {
                 return response()->json([
                     'success' => false,
                     'message' => 'Error por campos vacíos',
                 ], 401);
+            }
+
+            if ($newPassword != $confirmPassword) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Las contraseñas no coinciden',
+                ], 401);
+            }
+
+            if (!Hash::check($currentPassword, Auth::user()->password)) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'La contraseña actual no coincide',
+                ], 401);
+            }
+
+            DB::beginTransaction();
+            $user = User::find(Auth::user()->id);
+            $user->password = Hash::make($newPassword);
+            $user->save();
+            DB::commit();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Contraseña actualizada',
+            ], 200);
         } catch (\Exception $e) {
             DB::rollback();
             return response()->json([
