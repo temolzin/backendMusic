cd4927023a41fe6896081cf04691ff19aae3810c
SJAmanda <amandajacobo10@gmail.com>
2026-06-01 18:06:08 -0600

    Update CardSeeder to assign a unique card to each client

--- database/seeders/CardSeeder.php
+++ database/seeders/CardSeeder.php
@@ -22,10 +22,21 @@ class CardSeeder extends Seeder
             $query->where('slug', User::ROLE_CLIENT);
         })->orderBy('id')->get();
 
+        $cards = [
+            ['type' => 'Visa', 'number' => '4111111111111111'],
+            ['type' => 'Mastercard', 'number' => '5555555555554444'],
+            ['type' => 'American Express', 'number' => '378282246310005'],
+            ['type' => 'Discover', 'number' => '6011111111111117'],
+            ['type' => 'Visa', 'number' => '4012888888881881'],
+            ['type' => 'Mastercard', 'number' => '5105105105105100'],
+        ];
+
         foreach ($users as $index => $user) {
+            $card = $cards[$index % count($cards)];
+
             Client::create([
                 'user_id' => $user->id,
-                'number_card' => '4111111111111111',
+                'number_card' => $card['number'],
                 'name' => $user->name,
                 'expiration_date' => '12/28',
             ]);
