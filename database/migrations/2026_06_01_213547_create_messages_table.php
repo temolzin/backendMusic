e386a6b944f8873435ffd8324fc7e1d94e6feb93
isr4elsabi <133062000+isr4elsabi@users.noreply.github.com>
2026-06-02 12:30:22 -0600

    add CASCADE to drop table statement in messages migration

--- database/migrations/2026_06_01_213547_create_messages_table.php
+++ database/migrations/2026_06_01_213547_create_messages_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -20,6 +21,6 @@ return new class extends Migration
 
     public function down()
     {
-        Schema::dropIfExists('messages');
+        DB::statement('DROP TABLE IF EXISTS messages CASCADE');
     }
 };
