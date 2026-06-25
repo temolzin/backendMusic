70190763c95d6a011122e44811fb995a2e53c056
SJAmanda <amandajacobo10@gmail.com>
2026-06-25 13:58:04 -0600

    Add cascade constraint to musical genders migration

--- database/migrations/2026_06_25_102128_add_image_to_musical_genders_table.php
+++ database/migrations/2026_06_25_102128_add_image_to_musical_genders_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -15,8 +16,6 @@ return new class extends Migration
 
     public function down()
     {
-        Schema::table('musical_genders', function (Blueprint $table) {
-            $table->dropColumn('image');
-        });
+        DB::statement('ALTER TABLE musical_genders DROP COLUMN image CASCADE');
     }
 };
