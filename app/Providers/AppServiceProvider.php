a145fcfa7119878e758aa61e7562927720226140
Lowdy28 <2516360032icastillon@gmail.com>
2026-07-01 12:24:17 -0600

    refactor(console): delegate approvals expiration schedule to AppServiceProvider resolving callback

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->resolving(Schedule::class, function (Schedule $schedule) {
+            $schedule->command('approvals:expire')->hourly();
+        });
     }
 }
