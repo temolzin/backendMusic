14811e1ac1926c5486a75698a7b15a8080f7add7
SJAmanda <amandajacobo10@gmail.com>
2026-07-20 17:18:19 -0600

    Remove non-existent fields from artist video model

--- app/Models/ArtistVideo.php
+++ app/Models/ArtistVideo.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class ArtistVideo extends Model
 {
-    protected $fillable = ['artist_id', 'youtube_url', 'title', 'thumbnail'];
+    protected $fillable = ['artist_id', 'youtube_url'];
 }
