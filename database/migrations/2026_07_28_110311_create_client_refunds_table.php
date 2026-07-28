079f72da39582b032dbbec2f4a7f07f05692643d
Alan Cid <alanciddiaz014@gmail.com>
2026-07-28 14:17:49 -0600

    fix: add correct identation

--- database/migrations/2026_07_28_110311_create_client_refunds_table.php
+++ database/migrations/2026_07_28_110311_create_client_refunds_table.php
@@ -7,33 +7,33 @@ use Illuminate\Support\Facades\DB;
 
 class CreateClientRefundsTable extends Migration
 {
-  /**
-   * Run the migrations.
-   *
-   * @return void
-   */
-  public function up()
-  {
-    Schema::create('client_refunds', function (Blueprint $table) {
-      $table->id();
-      $table->foreignId('event_cancellation_id')->constrained('event_cancellations')->onDele('cascade');
-      $table->foreignId('customer_id')->constrained('users')->onDelete('cascade');
-      $table->foreignId('authorized_by')->nullable()->constrained('users')->onDelete('set null');
-      $table->decimal('refund_percentage', 5, 2);
-      $table->decimal('refund_amount', 10, 2);
-      $table->string('openpay_refund_id')->nullable();
-      $table->string('status')->default('pending');
-      $table->timestamps();
-    });
-  }
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_refunds', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_cancellation_id')->constrained('event_cancellations')->onDele('cascade');
+            $table->foreignId('customer_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('authorized_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->decimal('refund_percentage', 5, 2);
+            $table->decimal('refund_amount', 10, 2);
+            $table->string('openpay_refund_id')->nullable();
+            $table->string('status')->default('pending');
+            $table->timestamps();
+        });
+    }
 
-  /**
-   * Reverse the migrations.
-   *
-   * @return void
-   */
-  public function down()
-  {
-    DB::statement('DROP TABLE IF EXISTS client_refunds CASCADE;');
-  }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('DROP TABLE IF EXISTS client_refunds CASCADE;');
+    }
 }
