0558b8024d64b0c98c5c1ac770c93015189b3c33
Alan Cid <alanciddiaz014@gmail.com>
2026-07-28 13:22:46 -0600

    feature(model): create ClientRefund.php to the buisness logic

---
+++ app/Models/ClientRefund.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClientRefund extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'event_cancellation_id',
+        'customer_id',
+        'authorized_by',
+        'refund_percentage',
+        'refund_amount',
+        'openpay_refund_id',
+        'status',
+    ];
+
+    public function cancellation()
+    {
+        return $this->belongsTo(EventCancellation::class, 'event_cancellation_id');
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(User::class, 'customer_id');
+    }
+
+    public function authorizedBy()
+    {
+        return $this->belongsTo(User::class, 'authorized_by');
+    }
+}
