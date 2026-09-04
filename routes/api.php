9a602cc283ace49fc93fe8d017988b7a3ec09dac
SJAmanda <amandajacobo10@gmail.com>
2026-09-04 12:15:19 -0600

    refactor: move artist ratings route with artist routes

--- routes/api.php
+++ routes/api.php
@@ -150,11 +150,11 @@ Route::get('/google-maps-key', [GoogleMapsController::class, 'getKey']);
 //Route for General
 Route::get('/latest-artists', [ArtistsGeneralController::class, 'latestArtists']);
 Route::get('/system-comments', [SystemCommentController::class, 'index']);
-Route::get('/artist/{artistId}/ratings', [ArtistRatingController::class, 'listArtistRatings']);
 // Test route
 Route::resource('/product', ProductController::class);
 
 Route::get('/artist/getArtist', [ArtistController::class, 'getArtist']);
+Route::get('/artist/{artistId}/ratings', [ArtistRatingController::class, 'listArtistRatings']);
 
 Route::post('/users-subscribe/send', [UsersSubscribeController::class, 'sendEmailToSubscribers'])
     ->middleware(['auth:api', 'can:send-newsletters']);
